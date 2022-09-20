d0c5187cb4db6cead9e0aedc658207a5e1d43ea5
Juliana S <julianabrunahh@gmail.com>
2022-09-19 23:20:02 -0300

    att1
    Atividade consiste em escrever o nome na tela

---
+++ React/Modulo 2/my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Index from './componentes/index.jsx';
+
+function App() {
+  return (
+    <div>
+      <Index/>   
+    </div>
+  );
+}
+
+export default App;
---
+++ React/Modulo 2/my-app/src/componentes/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './index.css';
+
+export default function Index(){
+    return (
+        <section>
+            <div className="container">
+                <h1><i>Juliana Bruna</i></h1>
+            </div>
+        </section>
+    )
+}
