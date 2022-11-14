e9228d77a8b7246bfdf15444dcfb9479a296c9c2
Juliana S <julianabrunahh@gmail.com>
2022-11-14 13:52:44 -0300

    Alterações no header e footer
    Alterações no header:
    background, nav, e elementos do header

--- React/Modulo 3/att2/my-app/src/componentes/header/header.js
+++ React/Modulo 3/att2/my-app/src/componentes/header/header.js
@@ -1,6 +1,6 @@
 import {useRef} from 'react'
 import './header.css'
-import img from './imagens/rec.png'
+import img from './imagens/flower.png'
 //import menuShow from './main'
 
 function Header(){
@@ -17,18 +17,10 @@ function Header(){
             <div class="logo">
             <img src={img} className="img" alt="logo" />
             </div>
-            <div class="menu-btn">
-            <button class="menu-btn" onClick={showNavbar}>
-                Menu
-            </button>
-            </div>
             
             <ul>
                 <li><a href="./index.html">Home</a></li>
-                <li><a href="./restaurantes/index.html">Restaurantes</a></li>
-                <li><a href="https://visit.recife.br/onde-ficar/hoteis/">Hoteis</a></li>
-                <li><a href="./recife/index.html">Recife Antigo</a></li>
-                <li><a href="https://visit.recife.br/agenda-recife/">Programação</a></li>
+                <li><a href="./restaurantes/index.html">Sobre</a></li>
                 <li><a href="#contato">Contato</a></li>
             </ul>
         </nav>
