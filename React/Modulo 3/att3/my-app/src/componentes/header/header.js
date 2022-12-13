2cf8e0861418f20560850eea2e12e6927a70f499
Juliana S <julianabrunahh@gmail.com>
2022-12-13 20:20:13 -0300

    Criação das rotas

--- React/Modulo 3/att3/my-app/src/componentes/header/header.js
+++ React/Modulo 3/att3/my-app/src/componentes/header/header.js
@@ -1,24 +1,34 @@
 import './header.css'
 import img from './imagens/flower.png'
-//import menuShow from './main'
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
 
 function Header(){
 
 
     return(
-        <div id='header'className="header">
+        <Router>
         <nav>
             <div class="logo">
                 <img src={img} className="img" alt="logo" />
             </div>
             
             <ul>
-                <li><a href="#header">Home</a></li>
-                <li><a href="#">Sobre</a></li>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/sobre">Sobre</Link>
+                </li>
                 <li><a href="#contato">Contato</a></li>
             </ul>
         </nav>
-        </div>
+        <Switch>
+            <Route exact path='/'>
+            </Route>
+            <Route path='/sobre'>
+            </Route>
+        </Switch>
+        </Router>
     )
 }
 export default Header
